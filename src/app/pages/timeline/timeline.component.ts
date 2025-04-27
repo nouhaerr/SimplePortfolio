babfce6edc735727b42a439c1f6c4be9b26fb5ab
Nouhaila ERRAKEB <nerrakeb@Nouhailas-MacBook-Pro.local>
2025-04-27 21:15:03 +0100

    timelineComponent

--- src/app/pages/timeline/timeline.component.ts
+++ src/app/pages/timeline/timeline.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-timeline',
   standalone: true, // 👈 this line
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.css'
 })
 export class TimelineComponent {
+  currentSection: 'studies' | 'experiences' = 'studies';
 
+  showSection(section: 'studies' | 'experiences') {
+    this.currentSection = section;
+  }
 }
